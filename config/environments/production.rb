9e820cc6b22368d02572fb35c20a1f023811844b
fumi-1218 <57382173+fumi-1218@users.noreply.github.com>
2019-12-04 11:30:03 +0900

    edit deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,8 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  server '18.176.84.42', user: 'ec2-user', roles: %w{app db web}
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.

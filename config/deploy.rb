e553640c80f51572193cbc48a131a1d015782c85
fumi-1218 <57382173+fumi-1218@users.noreply.github.com>
2019-12-04 17:01:53 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,7 +50,7 @@ namespace :deploy do
     invoke 'unicorn:start'
   end
 
-  desc 'upload secrets.yml'
+  desc 'upload credentials.yml.enc'
   task :upload do
     on roles(:app) do |host|
       if test "[ ! -d #{shared_path}/config ]"

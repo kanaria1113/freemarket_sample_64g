47d10cf9bc32a5739b55a164418bd8720057f3a1
fumi-1218 <57382173+fumi-1218@users.noreply.github.com>
2019-12-04 12:22:17 +0900

    edit deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,6 @@
+server '18.176.84.42', user: 'ec2-user', roles: %w{app db web}
+
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
@@ -60,4 +63,3 @@
 #     # password: "please use keys"
 #   }
 
-server '18.176.84.42', user: 'ec2-user', roles: %w{app db web}

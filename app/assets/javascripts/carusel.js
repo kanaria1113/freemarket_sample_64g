6bea74350dd6c1c168be5437b17b719c26bac8d0
fumi-1218 <57382173+fumi-1218@users.noreply.github.com>
2019-12-05 21:39:18 +0900

    create carusel
    カルーセルスライダーの作成をした

---
+++ app/assets/javascripts/carusel.js
@@ -0,0 +1,13 @@
+$(document).on('turbolinks:load', function(){
+  $('.slick').slick({
+    autoplay: true,  
+    dots:true,
+  });
+});
+
+$(function(){
+  $("#slider").slick({
+    prevArrow: '<img src="前への矢印画像のパス" class="slide-arrow prev-arrow">',
+    nextArrow: '<img src="次への矢印画像のパス" class="slide-arrow next-arrow">'
+  });
+});

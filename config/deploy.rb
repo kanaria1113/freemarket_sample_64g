b0de743c6eb7fadb5cc733a0b5f8053828f6b1f3
fumi-1218 <57382173+fumi-1218@users.noreply.github.com>
2019-12-09 12:39:43 +0900

    edit deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,7 +40,8 @@ set :default_env, {
   AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
 }
 
-set :linked_files, fetch(:linked_files, []).push("config/master.key")
+set :linked_files, %w{ "config/master.key" }
+
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
--- db/schema.rb
+++ db/schema.rb
@@ -17,4 +17,13 @@ ActiveRecord::Schema.define(version: 2019_12_03_100647) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
 end

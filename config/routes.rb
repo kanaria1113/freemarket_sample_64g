309987e8b6579c80515809cf1fe10bfe24d46342
kanaria1113 <kana1113@hotmail.co.jp>
2019-12-13 20:20:01 +0900

    会員登録

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'index#index'
+  root to: 'index#login'
+  
+  resources :registration, only: [:index] do
+    collection do
+      get 'registration_information'
+      get 'registration_active'
+      get 'registration_address'
+      get 'registration_payment'
+      get 'registration_complete'
+    end
+  end
 end

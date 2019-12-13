1c788d21b9b8360173bf0cdbf1c0311ea1414209
fumi-1218 <57382173+fumi-1218@users.noreply.github.com>
2019-12-13 21:54:29 +0900

    create profile
    プロフィール画面を作成した

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'index#index'
-  resources :mypage, only: [:index]
+  resources :mypage, only: [:index] do
+    get "profile"
+  end
   resources :logout, only: [:index]
   resources :sell, only: [:index]
 end

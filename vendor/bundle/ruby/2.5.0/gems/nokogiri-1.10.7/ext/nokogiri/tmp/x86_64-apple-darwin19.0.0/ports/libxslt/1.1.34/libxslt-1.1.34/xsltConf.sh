58a705b852d60c7420d7fd1deb10118aa03802eb
fumi-1218 <57382173+fumi-1218@users.noreply.github.com>
2019-12-13 19:12:19 +0900

    cteate sns sign up
    SNS認証でのサインアップ機能の作成を行った、テストはまだである

---
+++ vendor/bundle/ruby/2.5.0/gems/nokogiri-1.10.7/ext/nokogiri/tmp/x86_64-apple-darwin19.0.0/ports/libxslt/1.1.34/libxslt-1.1.34/xsltConf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/Users/sasakiyoshifumi/projects/freemarket_sample_64g/vendor/bundle/ruby/2.5.0/gems/nokogiri-1.10.7/ports/x86_64-apple-darwin19.0.0/libxslt/1.1.34/lib"
+XSLT_LIBS="-lxslt -L/Users/sasakiyoshifumi/projects/freemarket_sample_64g/vendor/bundle/ruby/2.5.0/gems/nokogiri-1.10.7/ports/x86_64-apple-darwin19.0.0/libxml2/2.9.10/lib -lxml2 -lz -L/usr/local/Cellar/xz/5.2.4/lib -llzma -lpthread -liconv -lm "
+XSLT_PRIVATE_LIBS=""
+XSLT_INCLUDEDIR="-I/Users/sasakiyoshifumi/projects/freemarket_sample_64g/vendor/bundle/ruby/2.5.0/gems/nokogiri-1.10.7/ports/x86_64-apple-darwin19.0.0/libxslt/1.1.34/include"
+MODULE_VERSION="xslt-1.1.34"

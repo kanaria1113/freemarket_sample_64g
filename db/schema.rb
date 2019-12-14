98221cb753224cee89fbd6b0f478c7f119217a77
matubarashinya <is2baseball1130@icloud.com>
2019-12-14 17:47:56 +0900

    edit

--- db/schema.rb
+++ db/schema.rb
@@ -44,22 +44,11 @@ ActiveRecord::Schema.define(version: 2019_12_14_062958) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "last_name", null: false
-    t.string "fast_name", null: false
-    t.string "last_name_kana", null: false
-    t.string "fast_name_kana", null: false
-    t.string "birth_year", null: false
-    t.string "birth_month", null: false
-    t.string "birth_day", null: false
-    t.string "nickname", null: false
-    t.text "profile"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.boolean "delete_flg", null: false
-    t.string "image"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

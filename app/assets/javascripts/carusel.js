e9a01c4adb69fb1757bf9616258b26b0861f7de4
fumi-1218 <57382173+fumi-1218@users.noreply.github.com>
2019-12-17 15:03:19 +0900

    商品詳細ページカルーセルの作成

--- app/assets/javascripts/carusel.js
+++ app/assets/javascripts/carusel.js
@@ -4,11 +4,32 @@ $(document).on('turbolinks:load', function(){
     autoplaySpeed: 5000,
     dots:true,
   });
-});
 
-$(function(){
-  $("#slider").slick({
-    prevArrow: '<img src="前への矢印画像のパス" class="slide-arrow prev-arrow">',
-    nextArrow: '<img src="次への矢印画像のパス" class="slide-arrow next-arrow">'
+  $(function(){
+    $("#slider").slick({
+      prevArrow: '<img src="前への矢印画像のパス" class="slide-arrow prev-arrow">',
+      nextArrow: '<img src="次への矢印画像のパス" class="slide-arrow next-arrow">'
+    });
+  });
+
+  $(function() {
+    console.log("aa")
+    $('.thumb-item').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.thumb-item-nav' //サムネイルのクラス名
+    });
+    $('.thumb-item-nav').slick({
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 5000,    
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        asNavFor: '.thumb-item', //スライダー本体のクラス名
+        focusOnSelect: true,
+    });
   });
 });

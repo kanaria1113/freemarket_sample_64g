7bc7e0b99f2aff35c2eb309be5ec16fbb9283067
fumi-1218 <57382173+fumi-1218@users.noreply.github.com>
2019-12-18 20:07:04 +0900

    profile_controllerの作成、viewの編集

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
   resources :logout, only: [:index]
   resources :items, only: [:index]
   resources :sell, only: [:index]
+  resources :profile, only: [:index]
   resources :card, only: [:index] do
     collection do
       get 'mypage_card_input'

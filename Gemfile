35b7105d80354ff72007620bb59daec689748492
masakiosawa <rssgboy1@gmail.com>
2019-12-19 17:03:04 +0900

    カテゴリーモデル作成

--- Gemfile
+++ Gemfile
@@ -88,4 +88,4 @@ gem "omniauth-google-oauth2"
 gem 'dotenv-rails'
 gem 'dropzonejs-rails', '~> 0.7.3'
 gem "gretel"
-
+gem 'ancestry'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_14_062958) do
+ActiveRecord::Schema.define(version: 2019_12_18_071130) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "postal_code", null: false
@@ -34,6 +34,14 @@ ActiveRecord::Schema.define(version: 2019_12_14_062958) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_cards_on_user_id"
+  end
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "ancestry"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
   create_table "indices", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|

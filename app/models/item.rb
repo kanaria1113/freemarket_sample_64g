eaa4db26123af2828e7fa8a0ab55b9bc8a162276
masakiosawa <rssgboy1@gmail.com>
2019-12-19 17:12:57 +0900

    itemモデル修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :seler, class_name: "User"
   belongs_to :buyer, class_name: "User"
+  belongs_to user, foreign_key: 'user_id'
+　belongs_to :category
   has_many :images
   validates :name, presence: true, length: { maximum: 40 }
   validates :description, presence: true, length: { maximum: 1000 }

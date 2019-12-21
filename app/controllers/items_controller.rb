8ed537c5b246118b0a38258711b7ead5c56d7a2d
masakiosawa <rssgboy1@gmail.com>
2019-12-21 20:54:28 +0900

    商品一覧トップページ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,23 @@
 class ItemsController < ApplicationController
-  before_action :item_params, only: [:show, :edit, :destroy]
+  before_action :item_params, only: [:index, :show, :edit, :destroy]
+
   def index
-    @items = Item.all
+    @items = Item.find(item_params[:id]).limit(10).order('created_at DESC')
+    @images = Image.find(item_params[:id])
+    @image = Image.find(item_id: image_params)
   end
   def show
     @category = Category.find(params[:id])
-    @images = Image.all
+    @images = Image.find(item_params[:id])
     @brand = Brand.find(params[:id])
-    @item = Item.find(item_params[:id])
+    @items = Item.find(item_params[:id])
   end
   
   private
   def item_params
     @item = Item.find(params[:id])
+  end
+  def image_params
+    @image = Image.find(params[:id])
   end
 end

860a66421fcbc2e15b6837c1fc1cd56ed3564f5a
matubarashinya <is2baseball1130@icloud.com>
2019-12-25 14:25:08 +0900

    edit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,40 +1,49 @@
 class ItemsController < ApplicationController
 
-  
+
   def index
     @items = Item.all
   end
+
   def edit
-    @item = Item.find(1)
+    @item = Item.find(set_item[:id])
     @image = @item.images.build
-    @image = Image.find(params[:id])
+
   end
+
   def show
     @category = Category.find(params[:id])
     @images = Image.all
     @brand = Brand.find(params[:id])
     @item = Item.find(item_params[:id])
   end
+
   def update
-    if @item.user_id == current_user.id && @item.update
+    if @item.seler_id == current_user.id && @item.update
       chenge_item_items_path
     else
-      chenge_item_items_path
     end
   end
+
   def destroy
     @item = Item.find(params[:id])
     @item.destroy
       redirect_to mypage_index_path
   end
+
   def chenge_item
     @item = Item.find(1)
     @image = Image.find(1)
     @images = @item.images
   end
+
   private
   def item_params
     params.require(:item).permit(:name,:price,:description,:status,:region,:burden,:date,:send_method,images_attributes: [:id,:image])
+  end
+  
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
 

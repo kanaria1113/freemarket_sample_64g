dce57f05a7e1b4a7df000a48dc9fc3000f194807
fumi-1218 <57382173+fumi-1218@users.noreply.github.com>
2019-12-26 16:50:23 +0900

    basic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+  before_action :basic_auth, if: :production?
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -9,9 +9,13 @@ class ApplicationController < ActionController::Base
   end
   private
 
+  def production?
+    Rails.env.production?
+  end
+  
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'nicknamef-g64' && password == '9898'
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
 
